fac5b8fc7c91e4e19b86ca385cc869ae25d69aa0
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-03-27 12:51:46 +0100

    URL shortener

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Link_shortener from './components/Link_shortener'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-center text-4xl font-[700] text-white underline mb-[1.5rem]'>URL SHORTENER WEBSITE</h1>
+      <Link_shortener/>
+    </div>
+  )
+}
+
+export default App
